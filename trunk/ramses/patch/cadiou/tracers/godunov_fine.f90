d690a47efa1199a5530dc976d38bf55e4bcf78e6
Corentin Cadiou <corentin.cadiou@cphyc.me>
2016-10-25 14:20:37 +0200

    First draft for AMR

--- trunk/ramses/patch/cadiou/tracers/godunov_fine.f90
+++ trunk/ramses/patch/cadiou/tracers/godunov_fine.f90
@@ -826,14 +826,25 @@ subroutine move_tracers_oct(ind_grid, fluxes, ilevel)
   integer,dimension(1:nvector, 0:twondim) :: tmp_ncell
   integer, dimension(1:nvector) :: tmp_ind_cell
 
-  integer :: i, j, k, dir, dim, ison, iskip, icell, igrid_cell, ficell, ncell, ipart, new_icell, ix, iy, iz
+  integer :: i, j, k, dir, dim, ison, iskip, icell, igrid_cell, ficell, ncell, ipart, new_icell, ix, iy, iz, ii, jj, kk
   integer :: ixnc, iync, iznc
   integer :: dim0, nxny, dirm2, nx_loc
   real(kind=dp) :: scale, dx
-  real(kind=dp), dimension(1:3) :: x, xbound, skip_loc
+  real(kind=dp), dimension(1:3) :: x, xbound, skip_loc, disp
   integer :: npos, ind_fathergrid_ncell
+  logical :: ok
 
 
+  ! Maximum indexes when looping on cells
+#IF NDIM == 1
+  ixnc = 2; iync = 1; iznc = 1
+#ENDIF
+#IF NDIM == 2
+  ixnc = 2; iync = 2; iznc = 1
+#ENDIF
+#IF NDIM == 3
+  ixnc = 2; iync = 2; iznc = 2
+#ENDIF
   ! integer, save :: ncall = 0
 
   ! ncall = ncall + 1
@@ -866,6 +877,9 @@ subroutine move_tracers_oct(ind_grid, fluxes, ilevel)
      do j = 1, nvector
         tmp_ind_cell(j) = ind_cell(j, i)
      end do
+     ! print*, 'direction   ', i
+     ! print*, 'grid        ', ind_grid
+     ! print*, 'tmp_ind_cell', tmp_ind_cell
      call getnborfather(tmp_ind_cell, tmp_ncell, nvector, ilevel)
      do k = 0, twondim
         do j = 1, nvector
@@ -884,6 +898,7 @@ subroutine move_tracers_oct(ind_grid, fluxes, ilevel)
   do j = 1, nvector
      ! Loop over all particles
      ipart = headp(ind_grid(j))
+     if(debug) print*, 'particle', ipart, 'level', ilevel
 
      do i = 1, numbp(ind_grid(j))
 
@@ -896,23 +911,109 @@ subroutine move_tracers_oct(ind_grid, fluxes, ilevel)
               x(dim) = (xp(ipart, dim) / scale + skip_loc(dim) - xg(ind_grid(j), dim))/dx + 1.5d0
            end do
 
+           ! print*, 'xp', (xp(ipart, :) / scale + skip_loc(:)) / dx + 1.5d0
+           ! print*, 'xg', (xg(ind_grid(j), :)) / dx
+           ! print*, 'x ', x
+
+           ok = .true.
            ix = x(1)
+           if (real(ix, dp) /= x(1)) ok = .false.
 #IF NDIM > 1
            iy = x(2)
+           if (real(iy, dp) /= x(2)) ok = .false.
 #ENDIF
 #IF NDIM > 2
            iz = x(3)
+           if (real(iz, dp) /= x(3)) ok = .false.
 #ENDIF
-           ison = 1 + (ix-1) + 2*(iy-1) + 4*(iz-1)
+           ! Three cases for the location of the particle:
+           ! 1. in the center of a cell
+           !    -> do nothing
+           ! 2. in the center of the grid (the grid just got refined)
+           !    -> distribute the particle randomly into the grid (following the mass)
+           ! 3. in the center of a deleted cell
+           !    -> recenter the particle in the enclosing cell
 
-           iskip = ncoarse + (ison-1)*ngridmax
-           icell = iskip + ind_grid(j)
+           if (.not. ok) then ! cases 2 and 3
+              !TODO : project to nearest randomly
+              ! print*, x
+              if (all(x(1:ndim) == 1.5d0)) then ! case 2
+                 ! print*, 'case 2'
+                 ! compute grid mass
+                 mass = 0d0
+                 do ison = 1, twotondim
+                    iskip = ncoarse + (ison-1)*ngridmax
+                    icell = iskip + ind_grid(j)
+                    mass = mass + uold(icell, 1)
+                 end do
+
+                 ! randomly pick a destination for the particle
+                 call ranf(localseed, rand)
+                 ! print*, 'before', xp(ipart, :) / dx
+
+                 do ii = 1, ixnc
+                 do jj = 1, iync
+                 do kk = 1, iznc
+                    ison = 1 + (ii-1) + 2*(jj-1) + 4*(kk-1)
+                    iskip = ncoarse + (ison-1)*ngridmax
+                    icell = iskip + ind_grid(j)
+                    if (rand < uold(icell, 1) / mass) then
+                       print*, 'projecting', ipart, ilevel, 1+ii+2*jj+4*kk
+
+                       disp = ((/ii, jj, kk/)*2 - 3) * dx / 2d0
+                       do dim = 1, ndim
+                          xp(ipart, dim) = xp(ipart, dim) + disp(dim)
+                       end do
+                       ix = ii
+                       iy = jj
+                       iz = kk
+                       rand = 1
+                    else
+                       rand = rand - uold(icell, 1) / mass
+                    end if
+                 end do
+                 end do
+                 end do
+                 ! print*, 'after ', xp(ipart, :) / dx
+                 ison = 1 + (ix-1) + 2*(iy-1) + 4*(iz-1)
+
+                 iskip = ncoarse + (ison-1)*ngridmax
+                 icell = iskip + ind_grid(j)
+              else                                                ! case 3
+                 ! print*, 'case 3'
+                 print*, 'recentering', ipart, ilevel
+                 ! print*, 'before', xp(ipart, :) / dx
+                 ison = 1
+                 do dim = 1, ndim
+                    if (x(dim) < 1.5) then
+                       xp(ipart, dim) = (xg(ind_grid(j), dim) - skip_loc(dim) - dx/2)*scale
+                    else
+                       xp(ipart, dim) = (xg(ind_grid(j), dim) - skip_loc(dim) + dx/2)*scale
+                       ison = ison + 2**(dim-1)
+                    end if
+                 end do
+                 ! print*, 'after ', xp(ipart, :) / dx
+                 iz = (ison-1) / 4 + 1
+                 iy = (ison-1-4*(iz - 1)) / 2 + 1
+                 ix = (ison-1-4*(iz - 1)-2*(iy - 1)) + 1
+
+                 iskip = ncoarse + (ison-1)*ngridmax
+                 icell = iskip + ind_grid(j)
+              end if
+           else
+              ison = 1 + (ix-1) + 2*(iy-1) + 4*(iz-1)
+
+              iskip = ncoarse + (ison-1)*ngridmax
+              icell = iskip + ind_grid(j)
+           end if
+           ! print*, ison, icell, ix, iy, iz
 
            ! print*, 'computing flux', ipart
 
            ! Compute the outflux (<0)
            Fout = 0
            do dir = 1, twondim
+              ! print*, ix, iy, iz, j, ilevel
               call getFlux(dir, ix, iy, iz, j, fluxes, flux)
               if (flux < 0) Fout = Fout + flux
            end do
@@ -939,15 +1040,15 @@ subroutine move_tracers_oct(ind_grid, fluxes, ilevel)
                     ! 3. the neighbour cell is unrefined, of coarser level as current cell (neighbour grid == 0)
                     if (son(ind_ncell(j, ison, dir)) > 0) then                      ! case 1
                        ! The algorithm has already treated it, hasn't it?
-                       print*, 'case 1'
+                       ! print*, 'case 1'
                     else
                        if (ind_ngrid(j, dir) == 0) then                             ! case 3
                           ! get the center of the cell
-                          print*, 'case 3'
+                          ! print*, 'case 3'
                        else                                                         ! case 2
                           dirm2 = (dir - 1) / 2 + 1 ! 1,2->1 | 3,4->2 | 5,6->3
 
-                          print*, 'case 2'
+                          ! print*, 'case 2'
                           if (mod(dir, 2) == 1) then ! going left
                              xp(ipart, dirm2) = xp(ipart, dirm2) - dx
                           else                       ! going right
@@ -1046,4 +1147,3 @@ contains
 
 
 end subroutine move_tracers_oct
-
